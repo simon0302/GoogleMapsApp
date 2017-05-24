2575318e1ebaca757c5236064887068e22106e9b
simon0302 <sx0302@yahoo.com>
2017-05-24 11:58:10 -0700

    Born Here marker added + current location
    The current location does not work, so I will fix it later. The born
    here marker is initalized.

--- GoogleMapsApp/app/src/main/java/com/example/simon/googlemapsapp/MapsActivity.java
+++ GoogleMapsApp/app/src/main/java/com/example/simon/googlemapsapp/MapsActivity.java
@@ -1,7 +1,15 @@
 package com.example.simon.googlemapsapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,6 +21,12 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LatLng currentLocation;
+    private GoogleMap currentMap;
+    private Location loc;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +50,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
+
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng tucson = new LatLng(32.2217, -110.9625);
+        mMap.addMarker(new MarkerOptions().position(tucson).title("Born here"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(tucson));
+        Log.d("My Map", "home location works");
+
+
+        //current location
+        currentLocation = new LatLng(loc.getLatitude(), loc.getLongitude());
+        Log.d("My Map", "current location retrieved");
+        mMap.addMarker(new MarkerOptions().position(currentLocation).title("You are here"));
+
+
+
     }
+
+
+
+
+
 }
